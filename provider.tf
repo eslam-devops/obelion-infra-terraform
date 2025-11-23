c328386c01eef92f315b429b176a90f7cd4a3325
eslam-devops <eslamzain863@gmail.com>
2025-11-23 06:51:16 +0200

    Add provider.tf - AWS Terraform provider configuration

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# AWS Provider Configuration
+# تكوين مزود AWS - يحدد المنطقة والنسخة
+
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
